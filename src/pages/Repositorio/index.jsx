4fefaaa183b875af19e56c8884ef8de3ebd40fc0
JooJC <joojmoutinho45@gmail.com>
2023-10-06 13:49:57 -0300

    deploy fix

--- src/pages/Repositorio/index.jsx
+++ src/pages/Repositorio/index.jsx
@@ -75,7 +75,7 @@ export default function Repositorio() {
 
   return (
     <Container>
-      <BackButton to="/">
+      <BackButton to="/projeto_repos">
         <FaArrowLeft color="#000" size={30} />
       </BackButton>
       <Owner>
